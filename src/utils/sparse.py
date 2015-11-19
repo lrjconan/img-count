b4841d333a0f5f539ee169746193c3287017a1ac
Mengye Ren <renmengye@gmail.com>
2015-11-19 13:41:22 -0500

    sparse

---
+++ src/utils/sparse.py
@@ -0,0 +1,22 @@
+import scipy.sparse
+
+
+def read_h5(key):
+    pass
+
+
+def make_sparse_list(mat_list, batch_size=1000):
+    pass
+
+
+def make_sparse_batch(mat, batch_size=10000):
+    N = mat.shape[0]
+    num_bat = int(np.ceil(N / float(batch_size)))
+    feat_sparse = []
+    for i in xrange(num_bat):
+        start = bat * i
+        end = min(bat * (i + 1), N)
+        feat_copy = feat[start: end]
+        feat_sparse_tmp = scipy.sparse.csr_matrix(feat_copy)
+        feat_sparse.append(feat_sparse_tmp)
+    return scipy.sparse.vstack(feat_sparse, format='csr')
