a435124642a518aa51c0c4e1bea8acf6808c330c
Mengye Ren <renmengye@gmail.com>
2015-11-22 15:24:12 -0500

    running again

--- scripts/run_fast_rcnn_mscoco.sh
+++ scripts/run_fast_rcnn_mscoco.sh
@@ -1,30 +1,3 @@
-LOGTO="/ais/gobi3/u/mren/log/fast_rcnn_api" python src/fast_rcnn_api.py \
-    -image_list "/ais/gobi3/u/mren/data/mscoco/image_list_train.txt" \
-    -dataset "mscoco" \
-    -datadir "data/mscoco" \
-    -net "lib/fast-rcnn/models/VGG16/coco/test.prototxt" \
-    -weights "lib/fast-rcnn/data/fast_rcnn_models/coco_vgg16_fast_rcnn_iter_240000.caffemodel" \
-    -gpu $1 \
-    -conf 0.3 \
-    -nms 0.8 \
-    -local_feat \
-    -feat_layer "fc7" \
-    -proposal "/ais/gobi3/u/mren/data/mscoco/select_search_train.npy" \
-    -output "/ais/gobi3/u/mren/data/mscoco/fast_rcnn/fast_rcnn_c3n8_train" \
-    -num_images_per_shard 10000
-
-LOGTO="/ais/gobi3/u/mren/log/fast_rcnn_api" python src/fast_rcnn_api.py \
-    -image_list "/ais/gobi3/u/mren/data/mscoco/image_list_valid.txt" \
-    -dataset "mscoco" \
-    -datadir "data/mscoco" \
-    -net "lib/fast-rcnn/models/VGG16/coco/test.prototxt" \
-    -weights "lib/fast-rcnn/data/fast_rcnn_models/coco_vgg16_fast_rcnn_iter_240000.caffemodel" \
-    -gpu $1 \
-    -conf 0.3 \
-    -nms 0.8 \
-    -local_feat \
-    -feat_layer "fc7" \
-    -proposal "/ais/gobi3/u/mren/data/mscoco/select_search_valid.npy" \
-    -output "/ais/gobi3/u/mren/data/mscoco/fast_rcnn/fast_rcnn_c3n8_valid" \
-    -num_images_per_shard 10000
+scripts/run_fast_rcnn_mscoco_train.sh $1
+scripts/run_fast_rcnn_mscoco_valid.sh $1
 
--- scripts/run_fast_rcnn_mscoco_train.sh
+++ scripts/run_fast_rcnn_mscoco_train.sh
@@ -1,15 +1,15 @@
-LOGTO="/ais/gobi3/u/mren/log/fast_rcnn_api" python src/fast_rcnn_api.py \
+LOGTO="/ais/gobi3/u/mren/log/fast_rcnn_api" \
+    python src/fast_rcnn_api.py \
+    -gpu $1 \
     -image_list "/ais/gobi3/u/mren/data/mscoco/image_list_train.txt" \
     -dataset "mscoco" \
     -datadir "data/mscoco" \
     -net "lib/fast-rcnn/models/VGG16/coco/test.prototxt" \
     -weights "lib/fast-rcnn/data/fast_rcnn_models/coco_vgg16_fast_rcnn_iter_240000.caffemodel" \
-    -gpu $1 \
     -conf 0.3 \
     -nms 0.8 \
-    -local_feat \
-    -feat_layer "fc7" \
+    -local_feat "fc7,pool5" \
     -proposal "/ais/gobi3/u/mren/data/mscoco/select_search_train.npy" \
-    -output "/ais/gobi3/u/mren/data/mscoco/fast_rcnn/fast_rcnn_c3n8_train" \
+    -output "/ais/gobi3/u/mren/data/mscoco/fast_rcnn/fast_rcnn_train_c3n8" \
     -num_images_per_shard 10000
 
