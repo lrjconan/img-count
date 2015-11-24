cd14c08f7bf12b8f3d9ddfd4696f5a9583bc4a27
Mengye Ren <renmengye@gmail.com>
2015-11-24 14:45:50 -0500

    sync

---
+++ scripts/run_mscoco_pack_count_data_valid.sh
@@ -0,0 +1,9 @@
+python src/mscoco_pack_count_data.py \
+    -input_info "/ais/gobi3/u/mren/data/coco-count/coco_count_info_valid-?????-of-00001.h5" \
+    -input_feature "/ais/gobi3/u/mren/data/mscoco/fast_rcnn/fast_rcnn_valid_gt-?????-of-00005.h5" \
+    -local_feat "pool5" \
+    -datadir "/ais/gobi3/datasets/mscoco" \
+    -set "valid" \
+    -num_ex_per_shard 10000 \
+    -output "/ais/gobi3/u/mren/data/coco-count/gt_pool5/coco_count_valid"
+
