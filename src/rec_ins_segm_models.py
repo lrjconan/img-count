cf213d947efdf61dcce68b68c2889925f3271d39
Mengye Ren <renmengye@gmail.com>
2016-02-20 16:59:47 -0500

    redo

--- src/rec_ins_segm_models.py
+++ src/rec_ins_segm_models.py
@@ -914,7 +914,7 @@ def get_orig_model(opt, device='/cpu:0', train=True):
                 h_cnn0_shape = tf.shape(h_cnn[0])
                 zeros1 = tf.zeros(tf.concat(
                     0, [h_cnn1_shape[0: 1], tf.constant([timespan]), h_cnn1_shape[1:]]))
-                zeros1 = tf.zeros(tf.concat(
+                zeros0 = tf.zeros(tf.concat(
                     0, [h_cnn0_shape[0: 1], tf.constant([timespan]), h_cnn0_shape[1:]]))
                 h_cnn1_reshape = tf.reshape(tf.expand_dims(
                     h_cnn[1], 1) + zeros1,
