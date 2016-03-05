8bf815773c6d4bd52d55db642276b440ada226d7
Mengye Ren <renmengye@gmail.com>
2016-03-04 21:20:16 -0500

    tanh

--- src/rec_ins_segm_models.py
+++ src/rec_ins_segm_models.py
@@ -1515,10 +1515,13 @@ def get_attn_model(opt, device='/cpu:0'):
             [-1, inp_width, attn_size])
         filters_y_all_inv = tf.transpose(filters_y_all, [0, 2, 1])
         filters_x_all_inv = tf.transpose(filters_x_all, [0, 2, 1])
+        # y_out = _extract_patch(
+        #     h_dcnn[-1] + 5.0, filters_y_all_inv, filters_x_all_inv, 1)
         y_out = _extract_patch(
             h_dcnn[-1] + 5.0, filters_y_all_inv, filters_x_all_inv, 1)
         y_out = 1.0 / attn_lg_gamma * y_out
-        y_out = tf.sigmoid(y_out - 5.0)
+        y_out = tf.max(tf.tanh(y_out), 0.0)
+        # y_out = tf.sigmoid(y_out - 5.0)
         # y_out_b = nn.weight_variable([1])
         # y_out = tf.sigmoid(y_out - tf.exp(y_out_b))
         y_out = tf.reshape(y_out, [-1, timespan, inp_height, inp_width])
