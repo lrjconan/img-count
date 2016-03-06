d7d773884f1bfdffefc342183d4d702f44410846
Mengye Ren <renmengye@gmail.com>
2016-03-06 18:02:49 -0500

    mscoco

--- src/data_api/mscoco.py
+++ src/data_api/mscoco.py
@@ -134,7 +134,7 @@ class MSCOCO(object):
                     category: int
                 }
         """
-        anns = self._coco.ImgToAnns[image_id]
+        anns = self._coco.imgToAnns[image_id]
         ann = anns[0]
         segm = ann['segmentation'][0]
         x = []
