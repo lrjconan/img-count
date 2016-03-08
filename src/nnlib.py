141c5c280cecfd5121d7473f26c2cb0693288750
Mengye Ren <renmengye@gmail.com>
2016-03-08 15:28:52 -0500

    2

--- src/nnlib.py
+++ src/nnlib.py
@@ -68,23 +68,23 @@ def batch_norm(n_out, scope='bn', affine=True):
         ema = tf.train.ExponentialMovingAverage(decay=0.999)
         # ema_apply_op = ema.apply([batch_mean, batch_var])
 
-    def run_bn(x, phase_train):
-        _batch_mean, _batch_var = tf.nn.moments(x, [0, 1, 2], name='moments')
-        _batch_mean.set_shape([n_out])
-        _batch_var.set_shape([n_out])
-        # ema_apply_op = ema.apply([batch_mean, batch_var])
-        ema_apply_op = ema.apply([_batch_mean, _batch_var])
-        ema_mean, ema_var = ema.average(batch_mean), ema.average(batch_var)
+        def run_bn(x, phase_train):
+            _batch_mean, _batch_var = tf.nn.moments(x, [0, 1, 2], name='moments')
+            _batch_mean.set_shape([n_out])
+            _batch_var.set_shape([n_out])
+            # ema_apply_op = ema.apply([batch_mean, batch_var])
+            ema_apply_op = ema.apply([_batch_mean, _batch_var])
+            ema_mean, ema_var = ema.average(batch_mean), ema.average(batch_var)
 
-        def mean_var_with_update():
-            with tf.control_dependencies([ema_apply_op]):
-                return tf.identity(_batch_mean), tf.identity(_batch_var)
+            def mean_var_with_update():
+                with tf.control_dependencies([ema_apply_op]):
+                    return tf.identity(_batch_mean), tf.identity(_batch_var)
 
-        mean, var = control_flow_ops.cond(phase_train,
-                                          mean_var_with_update,
-                                          lambda: (ema_mean, ema_var))
-        normed = tf.nn.batch_normalization(x, mean, var, beta, gamma, 1e-3)
-        return normed, batch_mean, batch_var, ema_mean, ema_var
+            mean, var = control_flow_ops.cond(phase_train,
+                                              mean_var_with_update,
+                                              lambda: (ema_mean, ema_var))
+            normed = tf.nn.batch_normalization(x, mean, var, beta, gamma, 1e-3)
+            return normed, batch_mean, batch_var, ema_mean, ema_var
 
     return run_bn
 
