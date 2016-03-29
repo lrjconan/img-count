9c748ad91a80e9d94596d83d2f2c54ca92c5613f
Mengye Ren <renmengye@gmail.com>
2016-03-29 19:42:06 -0400

    debug nan

--- src/ris_double_attn_model.py
+++ src/ris_double_attn_model.py
@@ -378,11 +378,16 @@ def get_model(opt, device='/cpu:0'):
                         tt2 + 1] = tf.expand_dims(h_gmlp[-1], 2)
 
             ctrl_out = cmlp(h_crnn[tt][-1])[-1]
-            # Restrict to (-1, 1)
-            attn_ctr_norm[tt] = tf.tanh(tf.slice(ctrl_out, [0, 0], [-1, 2]))
-            # Restrict to (-inf, 0)
-            attn_lg_size[tt] = -tf.nn.softplus(
-                tf.slice(ctrl_out, [0, 2], [-1, 2]))
+
+            attn_ctr_norm[tt] = tf.slice(ctrl_out, [0, 0], [-1, 2])
+            attn_lg_size[tt] = tf.slice(ctrl_out, [0, 2], [-1, 2])
+            
+            # # Restrict to (-1, 1)
+            # attn_ctr_norm[tt] = tf.tanh(tf.slice(ctrl_out, [0, 0], [-1, 2]))
+            # # Restrict to (-inf, 0)
+            # attn_lg_size[tt] = -tf.nn.softplus(
+            #     tf.slice(ctrl_out, [0, 2], [-1, 2]))
+            
             attn_ctr[tt], attn_size[tt] = get_unnormalized_attn(
                 attn_ctr_norm[tt], attn_lg_size[tt], inp_height, inp_width)
             attn_lg_var[tt] = tf.zeros(tf.pack([num_ex, 2]))
