fe94c478a73ba3d943571c54bad0c8ccff324912
Mengye Ren <renmengye@gmail.com>
2016-03-31 11:53:17 -0400

    debug

--- src/debug_nan.py
+++ src/debug_nan.py
@@ -14,8 +14,9 @@ log = logger.get()
 if __name__ == '__main__':
     # model_folder = sys.argv[1]
     # model_folder = '/ais/gobi3/u/mren/results/img-count/rec_ins_segm-20160329234848'
-    model_folder = '../results/rec_ins_segm-20160330212049'
-    data_folder = '/ais/gobi3/u/mren/data/lsc/A1'
+    # model_folder = '../results/rec_ins_segm-20160330212049'
+    model_folder = '../results/rec_ins_segm-20160331003617'
+    # data_folder = '/ais/gobi3/u/mren/data/lsc/A1'
     # model_folder = '../results/rec_ins_segm-20160329195154'
 
     saver = Saver(model_folder)
@@ -35,37 +36,37 @@ if __name__ == '__main__':
         model_opt['use_iou_box'] = True
 
     model = double_attention.get_model(model_opt)
-    all_data = cvppp.get_dataset(data_folder, data_opt)
-    dataset = {}
+    
+    # all_data = cvppp.get_dataset(data_folder, data_opt)
+    # dataset = {}
+    # split = 103
+    # random = np.random.RandomState(2)
+    # idx = np.arange(all_data['input'].shape[0])
+    # random.shuffle(idx)
+    # train_idx = idx[: split]
+    # valid_idx = idx[split:]
+    # log.info('Train index: {}'.format(train_idx))
+    # log.info('Valid index: {}'.format(valid_idx))
+    # dataset['train'] = {
+    #     'input': all_data['input'][train_idx],
+    #     'label_segmentation': all_data['label_segmentation'][train_idx],
+    #     'label_score': all_data['label_score'][train_idx]
+    # }
 
-    split = 103
-    random = np.random.RandomState(2)
-    idx = np.arange(all_data['input'].shape[0])
-    random.shuffle(idx)
-    train_idx = idx[: split]
-    valid_idx = idx[split:]
-    log.info('Train index: {}'.format(train_idx))
-    log.info('Valid index: {}'.format(valid_idx))
-    dataset['train'] = {
-        'input': all_data['input'][train_idx],
-        'label_segmentation': all_data['label_segmentation'][train_idx],
-        'label_score': all_data['label_score'][train_idx]
-    }
+    # batch_start = (step * 8) % train_idx.size
+    # batch_end = max(batch_start + 8, train_idx.size)
+    # x_bat = dataset['train']['input'][batch_start: batch_end]
+    # y_bat = dataset['train']['label_segmentation'][batch_start: batch_end]
+    # s_bat = dataset['train']['label_score'][batch_start: batch_end]
 
-    batch_start = (step * 8) % train_idx.size
-    batch_end = max(batch_start + 8, train_idx.size)
-    x_bat = dataset['train']['input'][batch_start: batch_end]
-    y_bat = dataset['train']['label_segmentation'][batch_start: batch_end]
-    s_bat = dataset['train']['label_score'][batch_start: batch_end]
+    # sess = tf.Session()
+    # saver.restore(sess, ckpt_fname)
 
-    sess = tf.Session()
-    saver.restore(sess, ckpt_fname)
-
-    loss = sess.run(model['loss'], feed_dict={model['x']: x_bat,
-                                              model['y_gt']: y_bat,
-                                              model['s_gt']: s_bat,
-                                              model['phase_train']: True})
-    log.info('loss: {:6.4f}'.format(loss))
+    # loss = sess.run(model['loss'], feed_dict={model['x']: x_bat,
+    #                                           model['y_gt']: y_bat,
+    #                                           model['s_gt']: s_bat,
+    #                                           model['phase_train']: True})
+    # log.info('loss: {:6.4f}'.format(loss))
 
     model_val = {}
     output_list = []
