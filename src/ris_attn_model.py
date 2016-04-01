9907c1772f3030a549a36ccadcd5e833dfd2fb28
Mengye Ren <renmengye@gmail.com>
2016-04-01 00:35:01 -0400

    stop gradient in double

--- src/ris_attn_model.py
+++ src/ris_attn_model.py
@@ -300,7 +300,7 @@ def get_model(opt, device='/cpu:0'):
             adcnn_init_w = [{'w': h5f['attn_dcnn_w_{}'.format(ii)][:],
                             'b': h5f['attn_dcnn_b_{}'.format(ii)][:]}
                            for ii in xrange(adcnn_nlayers)]
-            adcnn_frozen = [True] * adcnn_layers
+            adcnn_frozen = [True] * adcnn_nlayers
         else:
             adcnn_init_w = None
             adcnn_frozen = None
