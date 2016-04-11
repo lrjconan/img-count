ad2bc78f5076520db1448cf06dc69644a3c08441
Mengye Ren <renmengye@gmail.com>
2016-04-11 18:17:14 -0400

    debug

--- src/ris_train_base.py
+++ src/ris_train_base.py
@@ -4,6 +4,7 @@ Trainer functions.
 
 from __future__ import division
 
+import cv2
 import datetime
 import matplotlib
 matplotlib.use('Agg')
@@ -156,7 +157,7 @@ def plot_double_attention(fname, x, glimpse_map, max_items_per_row=9):
 
     f1, axarr = plt.subplots(num_row, num_col, figsize=(10, num_row))
     pu.set_axis_off(axarr, num_row, num_col)
-    
+
     for ii in xrange(num_ex):
         for tt in xrange(timespan):
             for jj in xrange(num_glimpse):
