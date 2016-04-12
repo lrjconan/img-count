cea5b0767cf19a425a059bb1d34e177834bfce08
Mengye Ren <renmengye@gmail.com>
2016-04-12 17:06:10 -0400

    cvppp split

--- src/nnlib.py
+++ src/nnlib.py
@@ -332,8 +332,8 @@ def cnn(f, ch, pool, act, use_bn, phase_train=None, wd=None, scope='cnn', model=
                     init_beta = None
                     init_gamma = None
 
-                with tf.variable_scope('layer_{}'.format(ii)):
-                    with tf.variable_scope('copy_{}'.format(copy[0])):
+                # with tf.variable_scope('layer_{}'.format(ii)):
+                #     with tf.variable_scope('copy_{}'.format(copy[0])):
                         h[ii], bm, bv, em, ev = batch_norm(
                             h[ii], out_ch, phase_train,
                             scope2='{}_{}_{}'.format(scope, ii, copy[0]),
@@ -482,8 +482,8 @@ def dcnn(f, ch, pool, act, use_bn, skip_ch=None, phase_train=None, wd=None, scop
                     init_beta = None
                     init_gamma = None
 
-                with tf.variable_scope('layer_{}'.format(ii)):
-                    with tf.variable_scope('copy_{}'.format(copy[0])):
+                # with tf.variable_scope('layer_{}'.format(ii)):
+                #     with tf.variable_scope('copy_{}'.format(copy[0])):
                         h[ii], bm, bv, em, ev = batch_norm(
                             h[ii], out_ch, phase_train,
                             scope2='{}_{}_{}'.format(scope, ii, copy[0]),
