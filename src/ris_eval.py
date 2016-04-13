92861e4dbd465882484c85637b98f80fc25a6519
Mengye Ren <renmengye@gmail.com>
2016-04-12 20:57:47 -0400

    test get variable

--- src/ris_eval.py
+++ src/ris_eval.py
@@ -75,7 +75,7 @@ def run_inference(sess, m, dataset, phase_train):
                                batch_size=batch_size,
                                get_fn=get_batch_fn(dataset),
                                cycle=False,
-                               progress_bar=phase_train)
+                               progress_bar=True)
     y_out = None
     s_out = None
     y_gt = None
