f705ed96283b5d97d26cdfac32d72bc32adde0d3
Mengye Ren <renmengye@gmail.com>
2016-04-14 17:41:12 -0400

    iou

--- src/ris_eval_base.py
+++ src/ris_eval_base.py
@@ -305,7 +305,7 @@ def run_eval(sess, m, dataset, batch_size=10, fname=None):
                                get_fn=get_batch_fn(dataset),
                                cycle=False,
                                progress_bar=True)
-    _run_eval(sess, output_list, batch_iter, analyzers)
+    _run_eval(sess, m, output_list, batch_iter, analyzers)
 
     # output_list = None
     # y_gt = [(np.random.rand(5, 5, 10, 10) > 0.5).astype('float')]
@@ -316,7 +316,7 @@ def run_eval(sess, m, dataset, batch_size=10, fname=None):
     # _run_eval(sess, output_list, batch_iter, analyzers)
 
 
-def _run_eval(sess, output_list, batch_iter, analyzers):
+def _run_eval(sess, m, output_list, batch_iter, analyzers):
     for x, y_gt, s_gt, idx in batch_iter:
         feed_dict = {m['x']: x, m['y_gt']: y_gt, m['phase_train']: False}
         r = sess.run(output_list, feed_dict)
